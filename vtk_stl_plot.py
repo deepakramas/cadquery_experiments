a1894eee05fd71ff340e8e3545bacccd7a370e07
deepakramas <deepak_ramas@hotmail.com>
2014-12-16 12:02:40 -0500

    function for plotting stl

--- vtk_stl_plot.py
+++ vtk_stl_plot.py
@@ -41,3 +41,25 @@ def vtk_show(renderer, width=400, height=300):
     data = str(buffer(writer.GetResult()))
     
     return Image(data)
+
+def plot_stl(filename, position=[0,0,1], viewAngle = 30, background = [1,1,1], opacity=0.25,width=400, height=300):
+    """
+    Plots stl files via vtk in Ipython
+    """
+    myCamera = vtk.vtkCamera()
+    myCamera.SetViewAngle(viewAngle)
+    myCamera.SetPosition(position[0],position[1],position[2])
+    mapper = vtk.vtkPolyDataMapper()
+    mesh = loadSTL(filename)
+    mapper.SetInput(mesh)
+
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+    actor.GetProperty().SetOpacity(opacity)
+
+    renderer = vtk.vtkRenderer()
+    renderer.AddActor(actor)
+    renderer.SetBackground(background[0], background[1], background[2])
+    renderer.SetActiveCamera(myCamera)
+    vtk_show(renderer)
+vtk_show(renderer)
