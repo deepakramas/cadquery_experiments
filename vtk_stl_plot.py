0fc6d03e08cd4719ac6730c977e70b2b1e5f0ee5
deepakramas <deepak_ramas@hotmail.com>
2014-12-16 11:46:24 -0500

    utility for VTK plotting in IPython

---
+++ vtk_stl_plot.py
@@ -0,0 +1,43 @@
+# Using code from http://nbviewer.ipython.org/urls/bitbucket.org/somada141/pyscience/raw/master/20140910_RayCasting/Material/PythonRayCastingSphereVTK.ipynb
+
+import os
+import vtk
+
+def loadSTL(filenameSTL):
+    readerSTL = vtk.vtkSTLReader()
+    readerSTL.SetFileName(filenameSTL)
+    # 'update' the reader i.e. read the .stl file
+    readerSTL.Update()
+
+    polydata = readerSTL.GetOutput()
+
+    # If there are no points in 'vtkPolyData' something went wrong
+    if polydata.GetNumberOfPoints() == 0:
+        raise ValueError(
+            "No point data could be loaded from '" + filenameSTL)
+        return None
+    
+    return polydata
+
+from IPython.display import Image
+def vtk_show(renderer, width=400, height=300):
+    """
+    Takes vtkRenderer instance and returns an IPython Image with the rendering.
+    """
+    renderWindow = vtk.vtkRenderWindow()
+    renderWindow.SetOffScreenRendering(1)
+    renderWindow.AddRenderer(renderer)
+    renderWindow.SetSize(width, height)
+    renderWindow.Render()
+     
+    windowToImageFilter = vtk.vtkWindowToImageFilter()
+    windowToImageFilter.SetInput(renderWindow)
+    windowToImageFilter.Update()
+     
+    writer = vtk.vtkPNGWriter()
+    writer.SetWriteToMemory(1)
+    writer.SetInputConnection(windowToImageFilter.GetOutputPort())
+    writer.Write()
+    data = str(buffer(writer.GetResult()))
+    
+    return Image(data)
